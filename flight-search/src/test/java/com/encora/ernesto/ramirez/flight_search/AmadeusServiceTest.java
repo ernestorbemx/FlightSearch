900960d0d13dd2d4711294939f9e5ebe79e1b604
ernestorbemx <ernesto.ramirez@encora.com>
2025-06-03 22:41:56 -0600

    Test access token generation

---
+++ flight-search/src/test/java/com/encora/ernesto/ramirez/flight_search/AmadeusServiceTest.java
@@ -0,0 +1,22 @@
+package com.encora.ernesto.ramirez.flight_search;
+
+import com.encora.ernesto.ramirez.flight_search.services.AmadeusService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+
+public class AmadeusServiceTest {
+
+    @Autowired
+    private AmadeusService service;
+
+    @Test
+    void accessTokenIsGenerated() {
+        String token = service.generateAccessToken().map(r -> r.getAccessToken()).block();
+        assertNotNull(token);
+    }
+}
