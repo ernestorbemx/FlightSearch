10eb76c820fa11423de2f4cf31af3516a6eea7de
ernestorbemx <ernesto.ramirez@encora.com>
2025-06-05 22:44:51 -0600

    Add missing entries to Dictionaries

--- flight-search/src/main/java/com/encora/ernesto/ramirez/flight_search/dtos/Dictionaries.java
+++ flight-search/src/main/java/com/encora/ernesto/ramirez/flight_search/dtos/Dictionaries.java
@@ -7,6 +7,24 @@ public class Dictionaries {
 
     private Map<String, LocationEntry> locations;
     private Map<String, String> aircraft;
+    private Map<String, String> currencies;
+    private Map<String, String> carriers;
+
+    public Map<String, String> getCurrencies() {
+        return currencies;
+    }
+
+    public void setCurrencies(Map<String, String> currencies) {
+        this.currencies = currencies;
+    }
+
+    public Map<String, String> getCarriers() {
+        return carriers;
+    }
+
+    public void setCarriers(Map<String, String> carriers) {
+        this.carriers = carriers;
+    }
 
     public Map<String, String> getAircraft() {
         return aircraft;
