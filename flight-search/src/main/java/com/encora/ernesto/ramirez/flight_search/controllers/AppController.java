2f19c09a792f362a44f54acc80a5f2b3bde74a50
ernestorbemx <ernesto.ramirez@encora.com>
2025-06-16 22:14:12 -0600

    Add localhost:3000 to cors policy

--- flight-search/src/main/java/com/encora/ernesto/ramirez/flight_search/controllers/AppController.java
+++ flight-search/src/main/java/com/encora/ernesto/ramirez/flight_search/controllers/AppController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequestMapping("/")
-@CrossOrigin(origins = {"http://localhost:8080", "http://127.0.0.1:8080", "http://127.0.0.1:5173", "http://localhost:5173"})
+@CrossOrigin(origins = {"http://localhost:8080", "http://127.0.0.1:8080", "http://127.0.0.1:5173", "http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:3000"})
 @RestController
 public class AppController {
 
